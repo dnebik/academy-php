b99d56c1e3656af2239cc2c80c5c80b88683171c
dneb <ya.dneb@yandex.ru>
2020-07-30 17:24:20 +0300

    fully change data on dunamic

--- lesson_3/data.php
+++ lesson_3/data.php
@@ -55,5 +55,50 @@ return [
             "skill" => "Photoshop",
             "level" => 10
         ]
-    ]
+    ],
+    "career" => "<p>Summarise your career here lorem ipsum dolor sit amet, consectetuer adipiscing elit. You can <a href=\"http://themes.3rdwavemedia.com/website-templates/orbit-free-resume-cv-template-for-developers/\" target=\"_blank\">download this free resume/CV template here</a>. Aenean commodo ligula eget dolor aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu.</p>",
+    "experiences" => [
+        [
+            "position" => "Lead Developer",
+            "place" => "Startup Hubs, San Francisco",
+            "discription" => "                        <p>Describe your role here lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo.</p>  
+                        <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. </p>",
+            "yearStart" => 2015,
+            "yearEnd" => "текущее время"
+        ],
+        [
+            "position" => "Senior Software Engineer",
+            "place" => "Google, London",
+            "discription" => "<p>Describe your role here lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.</p>",
+            "yearStart" => 2014,
+            "yearEnd" => 2015
+        ],
+        [
+            "position" => "UI Developer",
+            "place" => "Amazon, London",
+            "discription" => "<p>Describe your role here lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.</p>",
+            "yearStart" => 2012,
+            "yearEnd" => 2014
+        ]
+    ],
+    "project" => "<div class=\"intro\">
+                    <p>You can list your side projects or open source libraries in this section. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum et ligula in nunc bibendum fringilla a eu lectus.</p>
+                </div><!--//intro-->
+                <div class=\"item\">
+                    <span class=\"project-title\"><a href=\"#hook\">Velocity</a></span> - <span class=\"project-tagline\">A responsive website template designed to help startups promote, market and sell their products.</span>
+                    
+                </div><!--//item-->
+                <div class=\"item\">
+                    <span class=\"project-title\"><a href=\"http://themes.3rdwavemedia.com/website-templates/responsive-bootstrap-theme-web-development-agencies-devstudio/\" target=\"_blank\">DevStudio</a></span> - 
+                    <span class=\"project-tagline\">A responsive website template designed to help web developers/designers market their services. </span>
+                </div><!--//item-->
+                <div class=\"item\">
+                    <span class=\"project-title\"><a href=\"http://themes.3rdwavemedia.com/website-templates/responsive-bootstrap-theme-for-startups-tempo/\" target=\"_blank\">Tempo</a></span> - <span class=\"project-tagline\">A responsive website template designed to help startups promote their products or services and to attract users &amp; investors</span>
+                </div><!--//item-->
+                <div class=\"item\">
+                    <span class=\"project-title\"><a href=\"hhttp://themes.3rdwavemedia.com/website-templates/responsive-bootstrap-theme-mobile-apps-atom/\" target=\"_blank\">Atom</a></span> - <span class=\"project-tagline\">A comprehensive website template solution for startups/developers to market their mobile apps. </span>
+                </div><!--//item-->
+                <div class=\"item\">
+                    <span class=\"project-title\"><a href=\"http://themes.3rdwavemedia.com/website-templates/responsive-bootstrap-theme-for-mobile-apps-delta/\" target=\"_blank\">Delta</a></span> - <span class=\"project-tagline\">A responsive Bootstrap one page theme designed to help app developers promote their mobile apps</span>"
+
 ];
--- lesson_3/index.php
+++ lesson_3/index.php
@@ -5,6 +5,9 @@
     $languagesData = $data["languages"];
     $interestsData = $data["interests"];
     $skillsData = $data["skills"];
+    $careerData = $data["career"];
+    $experiencesData = $data["experiences"];
+    $projectData = $data["project"];
 ?>
 
 <!DOCTYPE html>
@@ -86,79 +89,35 @@
         <div class="main-wrapper">
             
             <section class="section summary-section">
-                <h2 class="section-title"><i class="fa fa-user"></i>Career Profile</h2>
+                <h2 class="section-title"><i class="fa fa-user"></i>Карьера</h2>
                 <div class="summary">
-                    <p>Summarise your career here lorem ipsum dolor sit amet, consectetuer adipiscing elit. You can <a href="http://themes.3rdwavemedia.com/website-templates/orbit-free-resume-cv-template-for-developers/" target="_blank">download this free resume/CV template here</a>. Aenean commodo ligula eget dolor aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu.</p>
+                    <? echo $careerData ?>
                 </div><!--//summary-->
             </section><!--//section-->
             
             <section class="section experiences-section">
-                <h2 class="section-title"><i class="fa fa-briefcase"></i>Experiences</h2>
-                
+                <h2 class="section-title"><i class="fa fa-briefcase"></i>Опыт</h2>
+
+                <? foreach ($experiencesData as $experience): ?>
                 <div class="item">
                     <div class="meta">
                         <div class="upper-row">
-                            <h3 class="job-title">Lead Developer</h3>
-                            <div class="time">2015 - Present</div>
+                            <h3 class="job-title"><? echo $experience["position"] ?></h3>
+                            <div class="time"><? echo $experience["yearStart"] ?> - <? echo $experience["yearEnd"] ?></div>
                         </div><!--//upper-row-->
-                        <div class="company">Startup Hubs, San Francisco</div>
+                        <div class="company"><? echo $experience["place"] ?></div>
                     </div><!--//meta-->
                     <div class="details">
-                        <p>Describe your role here lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo.</p>  
-                        <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. </p>
+                        <? echo $experience["discription"] ?>
                     </div><!--//details-->
                 </div><!--//item-->
-                
-                <div class="item">
-                    <div class="meta">
-                        <div class="upper-row">
-                            <h3 class="job-title">Senior Software Engineer</h3>
-                            <div class="time">2014 - 2015</div>
-                        </div><!--//upper-row-->
-                        <div class="company">Google, London</div>
-                    </div><!--//meta-->
-                    <div class="details">
-                        <p>Describe your role here lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.</p>  
-                        
-                    </div><!--//details-->
-                </div><!--//item-->
-                
-                <div class="item">
-                    <div class="meta">
-                        <div class="upper-row">
-                            <h3 class="job-title">UI Developer</h3>
-                            <div class="time">2012 - 2014</div>
-                        </div><!--//upper-row-->
-                        <div class="company">Amazon, London</div>
-                    </div><!--//meta-->
-                    <div class="details">
-                        <p>Describe your role here lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.</p>  
-                    </div><!--//details-->
-                </div><!--//item-->
+                <? endforeach; ?>
                 
             </section><!--//section-->
             
             <section class="section projects-section">
-                <h2 class="section-title"><i class="fa fa-archive"></i>Projects</h2>
-                <div class="intro">
-                    <p>You can list your side projects or open source libraries in this section. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum et ligula in nunc bibendum fringilla a eu lectus.</p>
-                </div><!--//intro-->
-                <div class="item">
-                    <span class="project-title"><a href="#hook">Velocity</a></span> - <span class="project-tagline">A responsive website template designed to help startups promote, market and sell their products.</span>
-                    
-                </div><!--//item-->
-                <div class="item">
-                    <span class="project-title"><a href="http://themes.3rdwavemedia.com/website-templates/responsive-bootstrap-theme-web-development-agencies-devstudio/" target="_blank">DevStudio</a></span> - 
-                    <span class="project-tagline">A responsive website template designed to help web developers/designers market their services. </span>
-                </div><!--//item-->
-                <div class="item">
-                    <span class="project-title"><a href="http://themes.3rdwavemedia.com/website-templates/responsive-bootstrap-theme-for-startups-tempo/" target="_blank">Tempo</a></span> - <span class="project-tagline">A responsive website template designed to help startups promote their products or services and to attract users &amp; investors</span>
-                </div><!--//item-->
-                <div class="item">
-                    <span class="project-title"><a href="hhttp://themes.3rdwavemedia.com/website-templates/responsive-bootstrap-theme-mobile-apps-atom/" target="_blank">Atom</a></span> - <span class="project-tagline">A comprehensive website template solution for startups/developers to market their mobile apps. </span>
-                </div><!--//item-->
-                <div class="item">
-                    <span class="project-title"><a href="http://themes.3rdwavemedia.com/website-templates/responsive-bootstrap-theme-for-mobile-apps-delta/" target="_blank">Delta</a></span> - <span class="project-tagline">A responsive Bootstrap one page theme designed to help app developers promote their mobile apps</span>
+                <h2 class="section-title"><i class="fa fa-archive"></i>Проекты</h2>
+                    <? echo $projectData ?>
                 </div><!--//item-->
             </section><!--//section-->
             
