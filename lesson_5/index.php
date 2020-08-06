11e3273bcdbf27a43ce8491c8315811b840d0ae0
dneb <ya.dneb@yandex.ru>
2020-08-06 13:11:24 +0300

    created 3 intarface and company info

--- lesson_5/index.php
+++ lesson_5/index.php
@@ -1,7 +1,26 @@
 <?php
-abstract class Product
+
+interface iInfo
 {
+    function info();
+}
+
+interface iCount
+{
+    function getCount();
+}
+
+interface iPrice
+{
+    function price();
+}
+
+abstract class Product implements iCount, iPrice, iInfo
+{
+    protected const YEAR_START = 2020;
+
     protected static $count;
+    protected static $companyName = "Сласти Индастри";
 
     protected $name;
     protected $price;
@@ -18,6 +37,16 @@ abstract class Product
         $this->getCount();
     }
 
+    function __get($name)
+    {
+        echo "Переменная '$name' вне доступа.";
+    }
+
+    function __set($name, $value)
+    {
+        echo "Невозможно присвоить '$value' преременной '$name', так как она находится вне доступа.";
+    }
+
     function info()
     {
         return [
@@ -27,12 +56,12 @@ abstract class Product
         ];
     }
 
-    function Price()
+    function price()
     {
         return $this->price . "<br>";
     }
 
-    function PriceWithVAT()
+    function priceWithVAT()
     {
         return $this->price + ($this->price / 100 * 20) . "<br>";
     }
@@ -40,6 +69,11 @@ abstract class Product
     function getCount()
     {
         echo "<p>Количество продукции: ". self::$count ."</p>";
+    }
+
+    static function showCompanyInfo()
+    {
+        echo "<p>Компания: " . self::$companyName . "</p><p>Год основания: " . self::YEAR_START . "</p>";
     }
 
     abstract protected function showImage();
@@ -63,7 +97,6 @@ class Chocolate extends Product
 
 class Candy extends Product
 {
-
     protected function showImage()
     {
         echo "<img src='Sweets_Candy.jpg' width='200' height='200'>";
@@ -72,3 +105,8 @@ class Candy extends Product
 
 $candy = new Candy("Восторг", 58.99, 10);
 $chocolate = new Chocolate("Радость", 120, 100, 1560);
+
+echo "<br><br>";
+
+Product::showCompanyInfo();
+Candy::showCompanyInfo();
