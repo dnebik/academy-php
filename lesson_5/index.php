ad13de7859e31f567bdbca1c2716d7fe685ce26c
dneb <ya.dneb@yandex.ru>
2020-08-06 11:35:19 +0300

    edit calss Product as abstract and create two extended classes

--- lesson_5/index.php
+++ lesson_5/index.php
@@ -1,17 +1,19 @@
 <?php
-class Product{
-    public $name;
-    public $price;
-    public $weight;
+abstract class Product
+{
+    protected $name;
+    protected $price;
+    protected $weight;
 
-    function __construct($name, $price, $weight)
+    function __construct(string $name, float $price, float $weight)
     {
         $this->name = $name;
         $this->price = $price;
         $this->weight = $weight;
     }
 
-    function info(){
+    function info()
+    {
         return [
             "name"=>$this->name,
             "weight"=>$this->weight,
@@ -19,34 +21,49 @@ class Product{
         ];
     }
 
-    function Price(){
+    function Price()
+    {
         return $this->price . "<br>";
     }
 
-    function PriceWithVAT(){
+    function PriceWithVAT()
+    {
         return $this->price + ($this->price / 100 * 20) . "<br>";
     }
-}
 
-function printInfo(Product $product){
-    $info = $product->info();
-    foreach ($info as $key=>$stat){
-        echo "$key: $stat <br>";
+    function showImage()
+    {
+
     }
 }
 
-$product_1 = new Product("auto", 1000000, 1600);
-$product_2 = new Product("brick", 50, 5);
-$product_3 = new Product("smartphone", 25000, 0.4);
+class Chocolate extends Product
+{
+    protected $calories;
 
-printInfo($product_1);
-echo "Цена без НДС:" . $product_1->Price();
-echo "Цена с НДС" . $product_1->PriceWithVAT() . "<br>";
+    public function __construct(string $name, float $price, float $weight, int $calories)
+    {
+        $this->calories = $calories;
+        parent::__construct($name, $price, $weight);
+    }
 
-printInfo($product_2);
-echo "Цена без НДС:" . $product_2->Price();
-echo "Цена с НДС" . $product_2->PriceWithVAT() . "<br>";
+    public function showImage()
+    {
+        echo "<img src='Chocolate.png' width='200' height='200'>";
+    }
+}
 
-printInfo($product_3);
-echo "Цена без НДС:" . $product_3->Price();
-echo "Цена с НДС" . $product_3->PriceWithVAT() . "<br>";
+class Candy extends Product
+{
+
+    public function showImage()
+    {
+        echo "<img src='Sweets_Candy.jpg' width='200' height='200'>";
+    }
+}
+
+$candy = new Candy("Восторг", 58.99, 10);
+$chocolate = new Chocolate("Радость", 120, 100);
+
+$candy->showImage();
+$chocolate->showImage();
