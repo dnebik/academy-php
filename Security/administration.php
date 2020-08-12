dec7c9961a9724c87dbfc82309ec5f69669201e9
dneb <ya.dneb@yandex.ru>
2020-08-12 15:28:48 +0300

    Administration file create

---
+++ Security/administration.php
@@ -0,0 +1,23 @@
+<?php
+
+session_start();
+
+if (!$_SESSION["login"])
+{
+    die();
+}
+
+?>
+
+<html>
+
+<head>
+    <title>Админестрирование</title>
+</head>
+
+<body>
+    <h1>Админестрирование сайта</h1>
+</body>
+
+
+</html>
