f6226b1460a55d795f3fec293ff9e823295ba759
dneb <ya.dneb@yandex.ru>
2020-08-12 16:41:33 +0300

    Login in administration and view of all new comments

--- Security/administration.php
+++ Security/administration.php
@@ -2,11 +2,17 @@
 
 session_start();
 
-if (!$_SESSION["login"])
-{
+if (!$_SESSION["login"]) {
     header("Location: login.php");
     die();
 }
+
+if ($_POST["exit"]) {
+    session_destroy();
+    header("Location: login.php");
+    die();
+}
+
 
 ?>
 
@@ -17,7 +23,34 @@ if (!$_SESSION["login"])
 </head>
 
 <body>
-    <h1>Админестрирование сайта</h1>
+<h1>Админестрирование сайта</h1>
+<hr>
+<form action="" method="post">
+    <?
+    $db = require_once("database.php");
+    $data = $db->prepare("SELECT * FROM security.comments WHERE moderation = 'new'");
+    $data->execute();
+    $comments = $data->fetchALL(PDO::FETCH_ASSOC);
+
+    foreach ($comments as $item) {
+        ?>
+        <select>
+            <option <?= ($_SESSION["level"] != "Admin") ? "selected" : "" ?> value="miss">Не трогать</option>
+            <? if ($_SESSION["level"] == "Admin") { ?>
+                <option selected value="true">Принять</option>
+            <? } ?>
+            <option value="false">Отказать</option>
+        </select>
+        <?
+    }
+    echo "<b>{$item["name"]}</b>: {$item["text"]}";
+    ?>
+    <input style="display: block; margin-top: 15px" type="submit" name="confirm" value="Подтвердить">
+</form>
+<hr>
+<form action="" method="post">
+    <input type="submit" value="Выход" name="exit">
+</form>
 </body>
 
 
