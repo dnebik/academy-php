437345f44b1e4477e444e1911b37f062631bde5c
dneb <ya.dneb@yandex.ru>
2020-08-13 12:10:46 +0300

    create new project

---
+++ Files/database.php
@@ -0,0 +1,20 @@
+<?php
+
+class database{
+    private static $link;
+
+    public static function getLink(){
+        if (self::$link) {
+            return self::$link;
+        }
+        else {
+            $data = parse_ini_file("settings.ini", true);
+            $databaseData = $data["database"];
+            $dsn = "{$databaseData["driver"]}:host={$databaseData["host"]};port={$databaseData["port"]};dbname={$databaseData["schema"]}";
+
+            self::$link = new PDO($dsn, $databaseData["username"], $databaseData["password"]);
+
+            return self::$link;
+        }
+    }
+}
---
+++ Files/index.php
@@ -0,0 +1,22 @@
+<?php
+
+
+
+?>
+
+<html>
+
+<head>
+    <title>Галерея</title>
+</head>
+
+<body>
+
+    <form action="" method="POST">
+        <input type="file" name="file">
+        <input type="submit">
+    </form>
+
+</body>
+
+</html>
